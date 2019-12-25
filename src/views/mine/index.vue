2459342b77c7e213cba488c2698c4757c53968fa
qaz2531883 <673307084@qq.com>
2019-12-25 15:05:55 +0800

    add createComponents

---
+++ src/views/mine/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="main">
+		<Header  title="我的喵喵"/>
+		<Login/>
+		<Tabbar/>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/Header'
+	import Tabbar from '@/components/Tabbar'
+	import Login from '@/components/Login'
+	export default {
+		name: 'mine',
+		components: {
+			Header,
+			Tabbar,
+			Login
+		}
+	}
+</script>
+
+<style>
+</style>
